49567178832ccef3836b00a5b5d8335d509cc667
Mikhail Yevchenko <m..@uo1.net>
2023-06-12 13:29:10 +0300

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gpt-context-maker",
-    version="0.1",
+    version="0.2-SNAPSHOT",
     packages=find_packages(),
     entry_points={
         'console_scripts': [
