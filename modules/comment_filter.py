8c07548f12d3d76a8717652e32e671e9c3f132cb
Mikhail Yevchenko <m..@uo1.net>
2023-06-12 09:04:34 +0300

    Use tokenizer for python comment removal

--- modules/comment_filter.py
+++ modules/comment_filter.py
@@ -1,29 +1,58 @@
 import re
+import tokenize
+import io
+
+def remove_comments_python(source_code):
+    io_obj = io.StringIO(source_code)
+    out = ""
+    prev_toktype = tokenize.INDENT
+    last_lineno = -1
+    last_col = 0
+
+    for tok in tokenize.generate_tokens(io_obj.readline):
+        token_type = tok[0]
+        token_string = tok[1]
+        start_line, start_col = tok[2]
+        end_line, end_col = tok[3]
+        ltext = tok[4]
+
+        if start_line > last_lineno:
+            last_col = 0
+        if start_col > last_col:
+            out += (" " * (start_col - last_col))
+
+        if token_type == tokenize.COMMENT:
+            pass
+        elif token_type == tokenize.STRING:
+            if prev_toktype != tokenize.INDENT:
+                if prev_toktype != tokenize.NEWLINE:
+                    out += token_string
+        else:
+            out += token_string
+        prev_toktype = token_type
+        last_col = end_col
+        last_lineno = end_line
+
+    return out
 
 def remove_comments(file, content=None):
     if content is None:
         with open(file, "r") as f:
             content = f.readlines()
-    
+
     file_string = ''.join(content)
 
-    # Set comment style based on file extension
-    if file.suffix in ['.py', '.java', '.c', '.cpp', '.cs']:
-        # For Python, Java, C, C++, C# we look for comments that start with //
-        # and /* */. We also look for Python-specific triple quote comments.
-        no_comments = re.sub(r"(#).*?$|/\*.*?\*/|\'\'\'.*?\'\'\'", " ", file_string, flags=re.DOTALL | re.MULTILINE)
+    if file.suffix == '.py':
+        no_comments = remove_comments_python(file_string)
+    elif file.suffix in ['.java', '.c', '.cpp', '.cs']:
+        no_comments = re.sub(r"//.*?$|/\*.*?\*/", " ", file_string, flags=re.DOTALL | re.MULTILINE)
     elif file.suffix in ['.js', '.ts']:
-        # For JavaScript and TypeScript, we look for comments that start with //
-        # and /* */
         no_comments = re.sub(r"//.*?$|/\*.*?\*/", " ", file_string, flags=re.DOTALL | re.MULTILINE)
     elif file.suffix in ['.php']:
-        # For PHP, we look for comments that start with //, /* */, and #
         no_comments = re.sub(r"//.*?$|/\*.*?\*/|#.*?$", " ", file_string, flags=re.DOTALL | re.MULTILINE)
     elif file.suffix in ['.swift']:
-        # For Swift, we look for comments that start with //, /* */, and ///
         no_comments = re.sub(r"//.*?$|/\*.*?\*/|///.*?$", " ", file_string, flags=re.DOTALL | re.MULTILINE)
     else:
-        # For all other files, we remove no comments.
         no_comments = file_string
 
     return no_comments.strip()
