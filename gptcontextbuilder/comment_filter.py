07ecfae186eb52f9a7bc5fd7129d69e7b899509a
Mikhail Yevchenko <m..@uo1.net>
2023-06-13 11:29:42 +0300

    Add a fallback for python comment remover

--- gptcontextbuilder/comment_filter.py
+++ gptcontextbuilder/comment_filter.py
@@ -4,36 +4,42 @@ import io
 
 
 def remove_comments_python(source_code):
-    io_obj = io.StringIO(source_code)
-    out = ""
-    prev_toktype = tokenize.INDENT
-    last_lineno = -1
-    last_col = 0
+    try:
+        io_obj = io.StringIO(source_code)
+        out = ""
+        prev_toktype = tokenize.INDENT
+        last_lineno = -1
+        last_col = 0
 
-    for tok in tokenize.generate_tokens(io_obj.readline):
-        token_type = tok[0]
-        token_string = tok[1]
-        start_line, start_col = tok[2]
-        end_line, end_col = tok[3]
+        for tok in tokenize.generate_tokens(io_obj.readline):
+            token_type = tok[0]
+            token_string = tok[1]
+            start_line, start_col = tok[2]
+            end_line, end_col = tok[3]
 
-        if start_line > last_lineno:
-            last_col = 0
-        if start_col > last_col:
-            out += (" " * (start_col - last_col))
+            if start_line > last_lineno:
+                last_col = 0
+            if start_col > last_col:
+                out += (" " * (start_col - last_col))
 
-        if token_type == tokenize.COMMENT:
-            pass
-        elif token_type == tokenize.STRING:
-            if prev_toktype != tokenize.INDENT:
-                if prev_toktype != tokenize.NEWLINE:
-                    out += token_string
-        else:
-            out += token_string
-        prev_toktype = token_type
-        last_col = end_col
-        last_lineno = end_line
+            if token_type == tokenize.COMMENT:
+                pass
+            elif token_type == tokenize.STRING:
+                if prev_toktype != tokenize.INDENT:
+                    if prev_toktype != tokenize.NEWLINE:
+                        out += token_string
+            else:
+                out += token_string
+            prev_toktype = token_type
+            last_col = end_col
+            last_lineno = end_line
 
-    return out
+        return out
+    except:  # noqa: E722
+        # fallback to regexp if tokenization fails due to syntax errors
+        regex = r"(^[ \t]*#.*?$|[ \t]+#[^'\"]+$)"
+        no_comments = re.sub(regex, "", source_code, flags=re.MULTILINE)
+        return no_comments
 
 
 def remove_comments(file, content):
